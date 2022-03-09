675220a273cfc09f63be4d764bde86f18b340d6d
Johnny <37770895+johnnymdoubleu@users.noreply.github.com>
2022-03-09 17:38:52 +0000

    finalised

--- solvers/proj_inf.m
+++
@@ -1,9 +0,0 @@
-function [y,rr] = proj_inf(x,lambda)
-if lambda < 0; 
-   error('Lambda needs to be nonegative');
-elseif lambda == 0; 
-   y = 0*x; 
-else
-   y = max(-lambda,min(x,lambda));
-end
-rr = (y == x); %(abs(y-x) < 1e-6*lambda);
